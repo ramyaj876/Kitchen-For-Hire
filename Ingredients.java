58466eb2b79fd44cfb7d64fa4baacbcd0bafb95d
Ramya J <Ramya J>
2017-02-23 15:09:51 +0530

    First commit

---
+++ Ingredients.java
@@ -0,0 +1,8 @@
+import io.realm.RealmObject;
+
+class Ingredients extends RealmObject
+{
+	String name;
+	String unit;
+	String unitQuantity;
+}
---
+++ User.java
@@ -0,0 +1,9 @@
+import io.realm.RealmObject;
+
+class User extends RealmObject
+{
+ 	String name;
+ 	long phoneNumber;
+ 	String address;
+ 	String email;   
+}
